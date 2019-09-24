9de8fa810af9572e3c84361b435bd3ec77029a29
Banka <bachna22@gmail.com>
2019-09-24 10:19:45 +0200

    Fixed displaying buttons

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { MatMenuModule } from "@angular/material/menu";
+
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatDividerModule } from "@angular/material/divider";
 import { WelcomeSignComponent } from "./welcome-sign/welcome-sign.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, WelcomeSignComponent],
+  declarations: [AppComponent, WelcomeSignComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
