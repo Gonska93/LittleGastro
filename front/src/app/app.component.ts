e03bb683837dc54745a546fb539b9ed640ce9e53
Banka <bachna22@gmail.com>
2019-09-24 16:17:51 +0200

    Added route for home and created home component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
+  template: `
+    <app-home></app-home>
+  `,
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
