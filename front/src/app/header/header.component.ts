e1cd808514d3c45d23dd7d4de0937d4b0aee4bd0
Banka <bachna22@gmail.com>
2019-09-24 16:19:09 +0200

    Added onCLick event for Register btn

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { RegisterModalComponent } from "../register-modal/register-modal.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClick() {
+    alert("WOW");
   }
 
+  register() {
+    console.log("sdssdsad");
+    this.dialog.open(RegisterModalComponent, {
+      height: "400px",
+      width: "400px"
+    });
+  }
 }
