6734099cc83cf48949f9584deb75de82bdcbf217
Gonska93 <gonska9933@gmail.com>
2019-09-26 11:51:55 +0200

    Add Exception folder which contains following: - ExceptionMessage class which holds all available exception messages for user - IllegalNameException class, - DbEntityOperationException class.

---
+++ Back/Back.Application/Exceptions/ExceptionMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Application.Exceptions
+{
+    public class ExceptionMessage
+    {
+        public const string NameAlreadyUsed = "Illegal name. Provided one might be already used."; 
+        public const string NoChanges = "Nothing has been changed. Operation cancelled."; 
+        public const string IncorrectId = "Illegal id. Entity with this id does not exist."; 
+        public const string UpdateFailed = "Could not update entity in the database."; 
+        public const string DeleteFailed = "Could not delete entity in the database."; 
+        public const string AddFailed = "Could not add entity in the database."; 
+        public const string SavingChangesFailed = "Could not save changes in the database."; 
+    }
+}
---
+++ Back/Back.Application/Exceptions/IllegalNameException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Application.Exceptions
+{
+    public class IllegalNameException : Exception
+    {
+        public IllegalNameException(string message) : base(message) { }
+        public IllegalNameException() : base(ExceptionMessage.NameAlreadyUsed) { }
+    }
+}
